30b24ddb10bb5530dad3281584500027b9d7d00b
Chris Moss <hcmoss70@gmail.com>
2022-10-04 23:18:02 -0400

    set up app component

---
+++ client/src/main.tsx
@@ -0,0 +1,13 @@
+console.log('Hello world')
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import App from './App'
+
+
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+         <App/> 
+    </React.StrictMode>
+  )
